32e1e7ea8cb6ceb378b37fb2bfc2eb75ab0bf5fd
Alex Honko <alex@itsdart.com>
2025-04-03 04:06:42 +0300

    Add uv and makefile

--- admin/make-api.sh
+++ admin/make-api.sh
@@ -4,6 +4,6 @@
 set -e
 
 rm -rf dart/generated
-openapi-python-client generate --url https://app.itsdart.com/api/v0/schema/
+uv run openapi-python-client generate --url https://app.itsdart.com/api/v0/public/schema/
 mv dart-api-client/dart_api_client dart/generated
 rm -rf dart-api-client
---
+++ makefile
@@ -0,0 +1,7 @@
+# Generate OpenAPI client.
+api:
+	admin/make-api.sh
+
+# Build a Python package and upload it to PyPI.
+deploy:
+	admin/make-deploy.sh
