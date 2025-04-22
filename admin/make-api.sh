726ced928489b26b1bcc67c719a9019fde66c8c5
Alex Honko <alex@itsdart.com>
2025-04-23 02:36:07 +0300

    Autogenerate API model imports

--- admin/make-api.sh
+++ admin/make-api.sh
@@ -9,24 +9,11 @@ mv dart-public-api-client/dart_public_api_client dart/generated
 rm -rf dart-public-api-client
 
 # Optimize API import paths
-cat <<EOF >> dart/generated/api/__init__.py
-from .comment import create_comment
-from .config import get_config
-from .dartboard import retrieve_dartboard
-from .doc import (
-    create_doc,
-    delete_doc,
-    list_docs,
-    retrieve_doc,
-    update_doc,
-)
-from .folder import retrieve_folder
-from .task import (
-    create_task,
-    delete_task,
-    list_tasks,
-    retrieve_task,
-    update_task,
-)
-from .view import retrieve_view
-EOF
+api_dir="dart/generated/api"
+init_file="$api_dir/__init__.py"
+
+find "$api_dir" -type f -name "*.py" ! -name "__init__.py" | while read -r file; do
+    model=$(basename "$(dirname "$file")")
+    action=$(basename "$file" .py)
+    echo "from .$model import $action" >> "$init_file"
+done
--- dart/generated/api/__init__.py
+++ dart/generated/api/__init__.py
@@ -3,19 +3,7 @@
 from .comment import create_comment
 from .config import get_config
 from .dartboard import retrieve_dartboard
-from .doc import (
-    create_doc,
-    delete_doc,
-    list_docs,
-    retrieve_doc,
-    update_doc,
-)
+from .doc import create_doc, delete_doc, list_docs, retrieve_doc, update_doc
 from .folder import retrieve_folder
-from .task import (
-    create_task,
-    delete_task,
-    list_tasks,
-    retrieve_task,
-    update_task,
-)
+from .task import create_task, delete_task, list_tasks, retrieve_task, update_task
 from .view import retrieve_view
